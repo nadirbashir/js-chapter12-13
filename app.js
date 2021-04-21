f58bcfaae6a349a82a9a45e2a5ddf888119955d1
nadirbashir <1.nadirbashir@gmail.com>
2021-04-21 16:57:12 +0500

    quest 1 updated

--- app.js
+++ app.js
@@ -1,52 +1,53 @@
 /*********Quest. 1 *****/
-// var inp = prompt("Enter any keyboard input");
-// if(inp.charCodeAt() >= 65  && inp.charCodeAt() <=90){
-//     alert("you've entered upper case letter")
-// }
-// else if(inp.charCodeAt() >= 97  && inp.charCodeAt() <=122){
-//     alert("you've entered lower case letter")
-// }
-// else{
-//     alert("you've entered a number")
-// }
+var charInput = prompt("Enter any character");
+
+if(charInput >= "A" && charInput <= "Z"){
+    alert("Uppercase");
+}
+else if(charInput >= "a" && charInput <= "z"){
+    alert("Lowercase");
+}
+else if(charInput >= 0){
+    alert("Number");
+}
 
 
 /**********Quest. 2 *******/
-// var num1 = prompt("Enter first number")
-// var num2 = prompt("Enter second number")
+var num1 = prompt("Enter first number")
+var num2 = prompt("Enter second number")
 
-// if(num1>num2){
-//     alert(num1+"is larger")
-// }
-    // else if(num1===num2){
-    //     alert("both numbers are equal")
-    // }
-// else{
-    //     alert(num2+"is larger")
-    // }
+if(num1>num2){
+    alert(num1+"is larger")
+}
+    else if(num1===num2){
+        alert("both numbers are equal")
+    }
+else{
+        alert(num2+"is larger")
+    }
     
     
     /*********Quest. 3 *****/
-    // var inp = prompt("Enter any number");
-    // if(inp > 0){
-    //     alert("you've entered a positive number")
-    // }
-    // else if(inp<0){
-    //     alert("you've entered a negative number")
-    // }
-    // else{
-    //     alert("you've entered zero")
-    // }
+    var inp = prompt("Enter any number");
+    if(inp > 0){
+        alert("you've entered a positive number")
+    }
+    else if(inp<0){
+        alert("you've entered a negative number")
+    }
+    else{
+        alert("you've entered zero")
+    }
     
 
     /*********Quest. 4 *****/
-    // var inp = prompt("Enter any alphabet");
-    // if(inp === 'a' || inp === 'e' || inp === 'i' || inp === 'o' || inp === 'u' || inp === 'A' || inp === 'E' || inp === 'I' || inp === 'O' || inp === 'U'){
-    //     alert("you've entered a vowel")
-    // }
-    // else{
-    //     alert("you've entered consonant")
-    // }
+    var inp = prompt("Enter any alphabet");
+    if(inp === 'a' || inp === 'e' || inp === 'i' || inp === 'o' || inp === 'u' || inp === 'A' || inp === 'E' || inp === 'I' || inp === 'O' || inp === 'U'){
+        alert("you've entered a vowel")
+    }
+    else{
+        alert("you've entered consonant")
+    }
 
 /************Quest. 5 *********/
 var password = "silent007";
@@ -62,13 +63,13 @@ else{
 }
 
     /***********Quest. 6 **********/
-    // var greeting; 
-    // var hour = 13; 
-    // if (hour < 18) { 
-    //     greeting = "Good day"; 
-    // }
-    //     else{
-    //     greeting = "Good evening"; }
+    var greeting; 
+    var hour = 13; 
+    if (hour < 18) { 
+        greeting = "Good day"; 
+    }
+        else{
+        greeting = "Good evening"; }
 
 /**************Quest. 7 ********/
 
